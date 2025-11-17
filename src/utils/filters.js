1bdc73caf296a1c46979bc54909c8f6d15c8e605
DESKTOP-L4FFAHV\amrnash <amrnashaat8@gmail.com>
2025-11-17 15:15:57 +0200

    bugfix: - fix filtering logic for GET /books to properly parsing genres and author

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,4 +1,4 @@
-import { buildRangeQueriesFromString } from "./stringUtils";
+import { buildRangeQueriesFromString } from "./stringUtils.js";
 
 export const buildBookFilters = (query) => {
   const filter = {};
@@ -7,16 +7,22 @@ export const buildBookFilters = (query) => {
     { queryParam: "publishedYear", fieldName: "publishedYear" },
   ];
   buildRangeQueriesFromString(query, expectedFields, filter);
-  if (query.genre) {
-    filter.genre = { $in: query.genre.split(",") };
+  // genre comes from frontend like this "genre[]".
+  if (query["genre[]"]) {
+    filter.genre = { $in: query["genre[]"] };
   }
   if (query.createdBy) {
     filter.createdBy = query.createdBy;
   }
-
+  if (query.rating) {
+    filter.averageRating = { $gt: query.rating };
+  }
+  if (query.author) {
+    const regex = new RegExp(`^${query.author}`, "i");
+    filter.author = { $regex: regex };
+  }
   if (query.q) {
     filter.$text = { $search: query.q };
   }
-
   return filter;
 };
